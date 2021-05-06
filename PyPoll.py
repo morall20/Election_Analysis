a1dead8f2516d8b0962478b088049512228e3b02
morall20 <82338072+morall20@users.noreply.github.com>
2021-05-06 14:56:22 -0400

    updated PyPoll.py
    Completed Module 3.4.5

--- PyPoll.py
+++ PyPoll.py
@@ -19,6 +19,8 @@ with open(file_to_load) as election_data:
      # To do: read and analyze the data here.
      # Read the file object with the reader function.
     file_reader = csv.reader(election_data)
-       # Print each row in the CSV file.
-    for row in file_reader:
-        print(row)
+
+    # Print the header row.
+    headers = next(file_reader)
+    print(headers)
+
